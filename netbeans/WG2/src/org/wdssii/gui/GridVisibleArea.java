3a673183ba16db1604a5639260b896560787c327
retoomey <retoomey@localhost>
2012-04-25 14:05:10 -0500

    Partial work for Bim's request #2.  Implement dragging/hilite/selection ability in the bscan virtual table.

--- netbeans/WG2/src/org/wdssii/gui/GridVisibleArea.java
+++ netbeans/WG2/src/org/wdssii/gui/GridVisibleArea.java
@@ -4,23 +4,42 @@ import java.awt.Rectangle;
 
 /** Object representing a square section of visible table area.
  * Used to render product outlines for readout in the 3D world, also to render table
- * tracking.
+ * tracking.  Used to handle rectangle selection.
  * 
  *  @author Robert Toomey
  *  
  */
 public class GridVisibleArea {
 
+    /** Number of rows hight the grid is */
     public int numRows;
+
+    /** Number of columns wide the grid is */
     public int numCols;
-    public int startCol; // Index of first visible column on screen
-    public int lastFullColumn; // Index of last fully (non-clipped column on
-    // screen)
-    public int lastPartialColumn; // Index of last clipped column on screen
+
+    /** Index of the first visible row on screen */
     public int startRow;
+
+    /** Index of first visible column on screen */
+    public int startCol; 
+
+    /* Index of last fully (non-clipped row on screen */
     public int lastFullRow;
+
+    /* Index of last clipped row on screen */
     public int lastPartialRow;
+
+    /* Index of last fully (non-clipped column on screen */
+    public int lastFullColumn;
+
+    /* Index of last clipped column on screen */
+    public int lastPartialColumn;
+
+    /** Possible clipping region of the viewport */
     public Rectangle clipBounds;
+
+    /** Active flag if selection set */
+    public boolean active;
     
     @Override
     public String toString() {

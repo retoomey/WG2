f14ebd86cfce100f9779daa00b8753b718096245
retoomey <retoomey@localhost>
2012-05-22 13:38:25 -0500

    Changes to deployment scripts.  Change to local directory so shortcuts work in linux.  Improve package script.

--- netbeans/WG2/util/package.pl
+++ netbeans/WG2/util/package.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use File::Copy;
 use File::Path;
-use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
+#use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
 
 # I should probably do this with ANT..gotta learn it better first though,
 # probably should get the whole build working with a generic ant file so it
@@ -10,7 +10,8 @@ use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
 # file on my windows box, lol.
 
 # The project root directory
-$phome = "/cygdrive/c/Users/Dyolf/Documents/NetBeansProjects/hg/netbeans/WG2";
+#$phome = "/cygdrive/c/Users/Dyolf/Documents/NetBeansProjects/hg/netbeans/WG2";
+$phome = "/home/rtoomey/NetBeansProjects/wg2/netbeans/WG2";
 
 # The location of the dist built directory
 $dist = "$phome/dist";
@@ -22,7 +23,7 @@ $native = "$phome/release";
 $w2config = "$phome/w2config";
 
 # The location of the util/run folder
-$w2config = "$phome/util/run";
+$util = "$phome/util/run/*";
 
 # Calculate directory based on current date/time
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)= localtime(time);
@@ -33,12 +34,35 @@ print "Packaging...$temp\n";
 rmtree($temp);
 mkdir($temp);
 copy ("$dist/WG2.jar", "$temp/WG2.jar");
-dircopy ("$dist/lib", "$temp/lib/");
-rcopy ("$native", "$temp/release");
-rcopy ("$w2config", "$temp/w2config");
-$temptar = $temp.".tar.gz";
-$tararg = "cvfz $temptar $temp";
-print "Tar is $tararg\n";
-`tar $tararg`;
+#dircopy ("$dist/lib", "$temp/lib/");
+
+# Copy libs from dist into archive folder
+`cp -r $dist/lib $temp/lib/`;
+
+# Copy native library tree into 'release' folder in archive folder
+#rcopy ("$native", "$temp/release");
+`cp -r $native $temp/release`;
+
+# Copy util scripts to root of archive
+#rcopy ("$w2config", "$temp/w2config");
+`cp -r $util $temp/`;
+
+# Copy default w2config folder to root of archive
+`cp -r $w2config $temp/w2config`;
+
+# Permissions...
+`chmod a+rx $temp/*.sh`;
+
+# Compress into a tar.gz archive
+#$temptar = $temp.".tar.gz";
+#$tararg = "cvfz $temptar $temp";
+#print "Tar is $tararg\n";
+#`tar $tararg`;
+
+# Compress into a zip archive
+# Windows compatible zip since not everyone installs 7-zip
+$tempzip = $temp.".zip";
+$ziparg = " -r $tempzip $temp";
+`zip $ziparg`;
 print "Success\n";
 
--- netbeans/WG2/util/run/mac.sh
+++ netbeans/WG2/util/run/mac.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# Get the directory of the SCRIPT and CD to it
+DIR="$(cd "$( dirname "$0" )" && pwd)"
+echo Moving into $DIR;
+cd $DIR;
+echo WG2 Starting...
 java -jar WG2.jar
